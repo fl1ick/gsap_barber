7866c013d2b5a98f2d7637e8ed66466f2c292410
Bagas Maulana <bagas.maulanarela@gmail.com>
2026-05-01 13:00:36 +0700

    maap 4

--- src/admin/pages/StroresAdmin.jsx
+++ src/admin/pages/StroresAdmin.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import uploadImage from "../../lib/UploadImage.js";
 import { useTable } from "../../hooks/useTable";
 import { uploadImage } from "../../lib/UploadImage";
 
